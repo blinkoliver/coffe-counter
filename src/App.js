308a3e52ccacbd35cf8b9b62346a1ad6d6940703
Anton Barouka <52159126+blinkoliver@users.noreply.github.com>
2020-04-19 22:09:22 +0300

    sort

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from "react";
 import "./App.css";
 import Counter from "../src/Counter";
-import Loading from "../src/Loading";
-import { useState } from "react";
 
 const App = (props) => {
-  const [loading] = useState(false);
-
-  return <div>{loading ? <Loading /> : <Counter />}</div>;
+  return <div>{<Counter/>}</div>;
 };
 
 export default App;
